c6c510cbaeb10ae9e933d9f8afb57cbeb96e27cb
AlpitNamdev <alpitnamdev577@gmail.com>
2021-11-30 12:03:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import SlotM from './Slotmachine'
+
+
+
+
+const App = () => {
+    return (
+        <>
+        <h1 className="heading_style"> 
+        🎰 Welcome to {" "}<span style = {{fontWeight:'bold'}}>Slot machine game 🎰</span>
+        </h1>
+        <div className="slotmachine">
+        <SlotM x = '😄' y = '😄' z = '😄' />
+        <SlotM x = '😄' y = '😄' z = '😄'/>
+        <SlotM x = '😄' y = '😄' z = '😄'/>
+
+        </div>
+        </>
+    )
+}
+
+export default App;
